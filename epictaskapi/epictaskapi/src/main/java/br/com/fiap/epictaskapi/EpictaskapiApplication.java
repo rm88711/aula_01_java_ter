425f04da84b16e251ae113f016ba09c5c67a896e
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-02 09:01:47 -0300

    epic

---
+++ epictaskapi/epictaskapi/src/main/java/br/com/fiap/epictaskapi/EpictaskapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.epictaskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpictaskapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpictaskapiApplication.class, args);
+	}
+
+}
